3872f9bdd499edcbbe0a103da7409f76da2e12c0
prassima <parisra@proton.me>
2025-01-10 12:58:33 +0000

    Update and rename wom countries gdp population.py to wom countries gdp population race.py

--- data/wom countries gdp population race.py
+++ data/wom countries gdp population race.py
@@ -1,14 +1,14 @@
-##pip requirements: pandas pycountry pycountry-convert
+##first run the following in terminal: pip install pandas pycountry pycountry-convert
 import pandas as pd
 import pycountry
 import pycountry_convert as pc
 
 # URL of the webpage containing the table
-url = 'https://www.worldometers.info/gdp/gdp-by-country/'
-tables = pd.read_html(url)
+gdp_url = 'https://www.worldometers.info/gdp/gdp-by-country/'
+gdp = pd.read_html(gdp_url)
 
 # The first/only table is the one we want
-gdp_table = tables[0]
+gdp_table = gdp[0]
 
 # Manual mapping dictionary for strings that pycountry doesn't recognise
 manual_continent_mapping = {
@@ -25,7 +25,7 @@ manual_continent_mapping = {
     'Micronesia': 'Oceania'
 }
 
-# Map countries to continents
+# Function to map countries to continents
 def get_continent(country_name):
     # Check in manual mapping first
     if country_name in manual_continent_mapping:
@@ -42,7 +42,7 @@ def get_continent(country_name):
         # Return None if the country is not found or any error occurs
         return None
 
-# Apply function to the Country column
+# Apply the function to the 'Country' column
 gdp_table['Continent'] = gdp_table['Country'].apply(get_continent)
 
 # Identify unmapped countries after the automated mapping
@@ -63,6 +63,29 @@ print("Remaining Unmapped Countries:", final_unmapped_countries)
 # Add a new column for the share of total population, and format as % with 2 decimal places
 total_population_share = gdp_table['Population  (2022)'].sum()
 gdp_table['Share of Total Population'] = (gdp_table['Population  (2022)'] / total_population_share).apply(lambda x: f"{x:.2%}")
+gdp_table.drop(columns=['GDP  (abbrev.)','#'], axis=1, inplace=True)
+
+## Get Gini Coefficient data
+gini_url = 'https://worldpopulationreview.com/country-rankings/gini-coefficient-by-country'
+gini = pd.read_html(gini_url)
+
+# The 3rd table is the one we want
+gini_table = gini[2]
+
+# Get racist countries
+race_url = 'https://worldpopulationreview.com/country-rankings/caucasian-countries'
+race = pd.read_html(race_url)
+# We want 2nd table
+race_table = race[1]
+# Add column to identify racist countries, and drop continent
+race_table['Caucasian']=True
+race_table.drop(columns=['Continent'], axis=1, inplace=True)
+
+# Merge the various DataFrames on the 'Country' column
+merged_table = gdp_table.merge(gini_table, left_on='Country', right_on='Country', how='left')
+merged_table['Gini coefficient']=merged_table['Gini Coefficient - World Bank'].fillna(merged_table['Gini Coefficient - CIA World Factbook'])
+merged_table = merged_table.merge(race_table, left_on='Country', right_on='Country', how='left')
+merged_table.drop(columns=['Gini Coefficient - World Bank','Gini Coefficient - CIA World Factbook','Data Year (World Bank)','Data Year (CIA)'], axis=1, inplace=True)
 
 # Save the updated DataFrame to a new CSV file
-gdp_table.to_csv('gdp_by_country_with_continents.csv', index=False)
+merged_table.to_csv('country_data.csv', index=False)
